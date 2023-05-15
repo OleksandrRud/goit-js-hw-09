da1dd880587dc3c49f9cf01e73dc7f4573ee9680
OleksandrRud <98171387+OleksandrRud@users.noreply.github.com>
2023-05-15 19:43:45 +0000

    Deploying to gh-pages from @ OleksandrRud/goit-js-hw-09@a5c213abdab506ecdb09686a4c4892f23ced7309 🚀

---
+++ 01-color-switcher.147aa859.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled",""),e.removeAttribute("disabled");const d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;r.style.background=t}),1e3);e.addEventListener("click",(function(){e.setAttribute("disabled",""),t.removeAttribute("disabled"),clearInterval(d)}))}));
+//# sourceMappingURL=01-color-switcher.147aa859.js.map
